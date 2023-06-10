6f1837fc1a0f0bdabe5bb989d9d73869d1b3f2eb
root <root@fe6fab5d83e4>
2023-06-10 18:31:14 +0000

    houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -9,37 +9,30 @@ import org.jointheleague.graphical.robot.Robot;
  *    Level 1
  */
 
-
-
-
-
 public class Houses {
-		
+
 	Robot rob = new Robot();
-	
-	int large=0;
-	
-	
-	
+
+	int large = 0;
+
 	public void run() {
 		
-		rob.setX(80);
-		rob.setY(400);
+		rob.setX(50);
+		rob.setY(100);
 		rob.penDown();
 		rob.setSpeed(100);
 		rob.setPenWidth(15);
-		
-		
-		for(int i=0; i<=10; i++)
-		{
+		rob.setWindowColor(0,0,0);
+	rob.setX(50);
+	rob.setY(400);
+	
+		for(int i=0; i<=9; i++){
 		
 		String size =	JOptionPane.showInputDialog("What size do you want the building to be");
 		 String color =		JOptionPane.showInputDialog("What color do you want the building to be");
-			String roof = 	JOptionPane.showInputDialog("What roof do you want the building to be");
-		
-		
-		
-		
+		String roof = "";
+		 
+
 		
 	
 		if(color.equals("red"))
@@ -86,9 +79,15 @@ public class Houses {
 			 large=250;
 		}
 		
+		 if(large==250)
+		 {
+			 roof= "straight";
+					 
+		 }
+		 else{
+				 roof = 	JOptionPane.showInputDialog("What roof do you want the building to be");
+				 }
 		
-			
-			
 		if(roof.equals("pointy"))
 		{
 			PointRoof();
@@ -98,70 +97,48 @@ public class Houses {
 		{
 			StraightRoof();
 		}
+			}
 		
-	    rob.move(180);
-		rob.setPenColor(0,255,0);
-		rob.move(100);
+		
 		
 		
 		}
-	
-	
-	
-	
-	
-	
-	}
-		
+
 	public void PointRoof()
-	
+
 	{
 		rob.move(large);
-		rob.turn(120);
+		rob.turn(60);
 		rob.move(30);
-		rob.turn(120);
+		rob.turn(60);
 		rob.move(30);
-		rob.turn(330);
+		rob.turn(60);
 		rob.move(large);
+		
+		rob.turn(270);
+		rob.move(11);
+		rob.setPenColor(1,160,32);
+		rob.move(40);
+		rob.turn(270);
 	}
-public void StraightRoof()
-	
+
+	public void StraightRoof()
+
 	{
-		
-	rob.move(large);
-	rob.turn(90);
-	rob.move(30);
-	rob.turn(90);
-	rob.move(large);
-	}	
-	
-	
-		
-	
-	
-		
-		
-		// Check the recipe to find out what code to put here
-	
-	
-	
-	
 
+		rob.move(large);
+		rob.turn(90);
+		rob.move(45);
+		rob.turn(90);
+		rob.move(large);
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		rob.turn(270);
+		rob.move(11);
+		rob.setPenColor(1,160,32);
+		rob.move(40);
+		rob.turn(270);
 	}
 
-	
-	
+	// Check the recipe to find out what code to put here
 
+}
