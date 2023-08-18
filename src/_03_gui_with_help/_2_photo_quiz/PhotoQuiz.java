3667dac78d6e31c9b7b078c676c01efe61f47d7d
root <root@08d2c72d08c3>
2023-08-18 01:25:34 +0000

    photoquiz

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,29 +26,34 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-String picture = "https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTNwWdVLoWRgaRzoyctQn8vnWiU_pUFth_L6AmYDVwfWXLwUloDa6j4VFYNT5AwZBEy";
+String picture = "https://t3.gstatic.cpictureom/licensed-image?q=tbn:ANd9GcTNwWdVLoWRgaRzoyctQn8vnWiU_pUFth_L6AmYDVwfWXLwUloDa6j4VFYNT5AwZBEy";
 		// 2. create a variable of type "Component" that will hold your image
 
 
 
 
-		Component comp = new Component();
+		Component comp = createImage(picture);
 		// 3. use the "createImage()" method below to initialize your Component
 
-		createImage();
+		
 
 		// 4. add the image to the quiz window
-
+quizWindow.add(comp);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
 
+String flavor =JOptionPane.showInputDialog("what flavor is the icecream");
 		// 7. print "CORRECT" if the user gave the right answer
 
 		
-		if(right)
+int score=0;
+
+		if(flavor == "Vanilla" || flavor == "vanilla" )
 		{
-			System.out.print("CORRECT");
+			JOptionPane.showMessageDialog(null,"CORRECT your score is "+score);
+			score++;
+		
 		}
 		
 		
@@ -58,26 +63,57 @@ String picture = "https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTNwWdVLoWRga
 
 		else
 		{
-			System.out.print("INCORRECT");
+			JOptionPane.showMessageDialog(null,"INCORRECT your score is "+score);
 		}
 		
 		
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+		
+		quizWindow.remove(comp);
+		
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
 		
+		String newpic = "https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTNwWdVLoWRgaRzoyctQn8vnWiU_pUFth_L6AmYDVwfWXLwUloDa6j4VFYNT5AwZBEy";
 		
-		
+		comp= createImage(newpic);
 
 		// 11. add the second image to the quiz window
 
+		
+		quizWindow.add(comp);
+		
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String car =JOptionPane.showInputDialog("what is the is the brand of this car");
 		// 14+ check answer, say if correct or incorrect, etc.
+
+
+
+
+if(car == "tesla" || car == "Tesla" )
+{
+	JOptionPane.showMessageDialog(null,"CORRECT, your score is "+score);
+	score++;
+	
+	
+}
+
+else
+{
+	JOptionPane.showMessageDialog(null,"INCORRECT your score is + "+score);
+}
+
+
+
+
+
+
+
+
 
 		
 		
