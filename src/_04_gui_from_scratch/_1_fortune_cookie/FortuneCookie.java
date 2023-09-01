4bf4f26541fa44143d017248cfd9bb4697c888cd
root <root@6f1ae66ae830>
2023-09-01 01:26:08 +0000

    Continue Jack in the Box

--- src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -1,5 +1,6 @@
 package _04_gui_from_scratch._1_fortune_cookie;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
@@ -8,56 +9,42 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class FortuneCookie {
-	
+public class FortuneCookie implements ActionListener {
+
 	public void showButton() {
-		
-		System.out.println("Button Clicked");
-		
-		
+
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
 		JButton button = new JButton();
-		
-		
-		
-		
-		
+
 		frame.setVisible(true);
-		
+
+		button.setText("Click me");
+
 		frame.add(button);
 		frame.pack();
-		
+
 		button.addActionListener(this);
-		
-		
-		
-		
-		
-		
+
 	}
 
-	
-	public actionPerformed  implements void ActionListener()
-	
-	{
-		
-		
-		
-		
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
 		JOptionPane.showMessageDialog(null, "Woohooo!");
-		
-		
-		Random rand = new Random();
-		
-		int num = rand*4;
-		
+
+		int rand = new Random().nextInt(5);
+
+		if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "Opportunities knock when you least expect them; keep your door open!");
+		} else if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "Your creativity knows no bounds; let it guide you to new horizons.");
+		} else if (rand == 3) {
+			JOptionPane.showMessageDialog(null,
+					"Learning from yesterday, living for today, and hoping for tomorrow brings success.");
+		} else {
+			JOptionPane.showMessageDialog(null, "The journey of a thousand miles begins with a single step, and you're already on the right path.");
+		}
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
